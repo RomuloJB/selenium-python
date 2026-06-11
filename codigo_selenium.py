5848efe6010a5e4d4fb48fa12bbd0cb8d9b7074b
RomuloJB <jordaobogoni@gmail.com>
2026-06-11 18:56:29 -0300

    fix | erro de sintaxe

--- codigo_selenium.py
+++ codigo_selenium.py
@@ -24,18 +24,27 @@ botao_rendimentos.send_keys("150000")
 
 botao_previdencia = navegador.find_element(By.ID, "mat-input-1")
 #fazer o selenium dar um scroll para baixo e encontrar e centralizar o elemento desejado
-navegador.execute_script("argument[0].scrollIntoView({block: 'center'})", botao_previdencia)
+navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", botao_previdencia)
 time.sleep(3)
 #campo previdencia
 navegador.find_element(By.ID, "mat-input-1").send_keys("1000")
 
+botao_dependentes = navegador.find_element(By.ID, "mat-input-2")
+navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", botao_dependentes)
+time.sleep(3)
 #campo dependentes
-#navegador.find_element(By.ID, "mat-input-2").send_keys("1000")
+navegador.find_element(By.ID, "mat-input-2").send_keys("1000")
 
+botao_pensao = navegador.find_element(By.ID, "mat-input-3")
+navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", botao_pensao)
+time.sleep(3)
 #campo pensao alimenticia
-#navegador.find_element(By.ID, "mat-input-3").send_keys("1000")
+navegador.find_element(By.ID, "mat-input-3").send_keys("1000")
 
+botao_outras_deducoes = navegador.find_element(By.ID, "mat-input-4")
+navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", botao_outras_deducoes)
+time.sleep(3)
 #campo outras deducoes
-#navegador.find_element(By.ID, "mat-input-4").send_keys("1000")
+navegador.find_element(By.ID, "mat-input-4").send_keys("1000")
 
 time.sleep(10)
